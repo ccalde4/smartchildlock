119f23e6e370cedb38ca97659516faa666cb2831
Austin <asteep1@lsu.edu>
2020-04-19 21:31:44 -0500

    Added a startup script to start all services. Adding it to the boot sequence.

--- RadioControl.py
+++ RadioControl.py
@@ -4,12 +4,12 @@ import spidev
 import RPi.GPIO as GPIO
 from lib_nrf24 import NRF24
 
+
 def init_radio():
     pipes=[[0xe8,0xe8,0xf0,0xf0,0xe1],[0xf0,0xf0,0xf0,0xf0,0xe1]]
     GPIO.setmode(GPIO.BCM)
     radio = NRF24(GPIO,spidev.SpiDev())
     radio.begin(0,17)
-    radio.setChannel(0x76)
     radio.setDataRate(NRF24.BR_2MBPS)
     radio.setPALevel(NRF24.PA_MAX)
     radio.setAutoAck(True)
@@ -17,7 +17,23 @@ def init_radio():
     radio.openWritingPipe(pipes[0])
     radio.openReadingPipe(1, pipes[1])
 
+def setup_lock(lock_name):
+    radio.setChannel(0x01)
+    init_radio()
+    radio_msg = list(lock_name)
+    while len(radio_msg)<32:
+        radio_msg.append(0)
+    radio.write(radio_msg)
+    start =time.time()
+    while not radio.available():
+        if(time.time()-start > 5):
+            return false
+        else:
+            return true
+
 def unlock_function(lock):
+    radio.setChannel(0x76)
+    init_radio()
     radio_msg = list(lock)
     while len(radio_msg)<32:
         radio_msg.append(0)
@@ -26,6 +42,6 @@ def unlock_function(lock):
     while not radio.available(0):
         time.sleep(1/100)
         if(time.time() - start > 2):
-            return_msg = "Timed out"
+            return false
         else:
-            return_msg = "Unlocked cabinet, {}".format(lock)
+            return true
---
+++ startup.sh
@@ -0,0 +1,5 @@
+lxterminal --command './ngrok http 8001'
+lxterminal --command './ngrok http 5000'
+lxterminal --command 'python3 ~/Desktop/smartchildlock/alexa.py'
+lxterminal --working-directory=/home/austin/Desktop/smartchildlock/webapp --command 'python3 webapprun.py'
+lxterminal --working-directory=/home/austin/Desktop/smartchildlock/webapp --command 'python3 time_monitor.py'
