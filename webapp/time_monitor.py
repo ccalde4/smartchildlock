2b6bc966974adedee009febb49fcf73025626302
Austin <asteep1@lsu.edu>
2020-04-20 10:25:54 -0500

    Added day of week function to time_monitor should be finished now

--- webapp/time_monitor.py
+++ webapp/time_monitor.py
@@ -9,6 +9,7 @@ def main():
     schedules = Schedule.query.all()
     start_times = [0]*len(schedules)
     end_times = [0]*len(schedules)
+    days = [0]*len(schedules)
     j=0
     for i in schedules:
         time = i.time
@@ -17,17 +18,37 @@ def main():
         end_time = time[2]
         start_times[j]=start_time
         end_times[j]=end_time
+        days[j]=i.date
         j=j+1
+    print(days)
     print(start_times)
     print(end_times)
+    day = datetime.today().weekday()
+    if(day == 0):
+        string_day = "Monday"
+    if(day == 1):
+        string_day = "Tuesday"
+    if(day == 2):
+        string_day = "Wednesday"
+    if(day == 3):
+        string_day = "Thursday"
+    if(day == 4):
+        string_day = "Friday"
+    if(day == 5):
+        string_day = "Saturday"
+    if(day == 6):
+        string_day = "Sunday"
     now = datetime.now()
     time = now.strftime("%H:%M")
     print(time)
     iterator = 0;
-    for i in start_times:
-        if(time == i):
-            schedule_start(iterator, schedules,end_times)
-        iterator = iterator +1
+    for i in days:
+        if(string_day in i):
+            print("Day present in schedule")
+            for j in start_times:
+                if(time == j):
+                    schedule_start(iterator, schedules,end_times)
+                iterator = iterator +1
     print("no start time matched time")
 
 def schedule_start(iterator, schedules, end_times):
